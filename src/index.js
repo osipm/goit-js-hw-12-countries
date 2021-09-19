466c9a8c36b43a1df3b85c1b8826c10a1333803e
osipm <54916138+osipm@users.noreply.github.com>
2021-09-19 22:34:00 +0300

    виконана

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import './sass/main.scss';
+import fetchCountries from './fetchCountries';
+import card from './card.hbs';
+import debounce from 'lodash.debounce';
+import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import list from './list.hbs'
+
+const dropdown = document.querySelector('.dropdown')
+
+
+const input = document.querySelector('#input');
+input.addEventListener('input', debounce(onSearchСountry, 1000));
+
+function onSearchСountry({target:{value}}) {
+    if(!value){return}
+    fetchCountries(value).then((response) => {
+        if (response.length === 1) {
+          dropdown.innerHTML = card(response[0]);
+        }
+        else if (response.length < 11) {
+          dropdown.innerHTML = list({ response });
+        }
+        else {
+          alert({
+            text: 'Notice me, senpai!',
+          });
+        }
+
+        
+
+    })
+
+
+
+}
+
+
