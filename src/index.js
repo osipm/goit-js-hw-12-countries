9805be1e7824d194f9171bae96bee2922fe38edb
osipm <54916138+osipm@users.noreply.github.com>
2021-09-20 23:22:27 +0300

    cathc

--- src/index.js
+++ src/index.js
@@ -13,22 +13,22 @@ input.addEventListener('input', debounce(onSearchСountry, 500));
 function onSearchСountry({target:{value}}) {
     if(!value){return}
     fetchCountries(value).then((response) => {
-        if (response.length === 1) {
-          dropdown.innerHTML = card(response[0]);
-        }
-        else if (response.length < 11) {
-          dropdown.innerHTML = list({ response });
-        }
-        else {
-          alert({
-            text: 'Notice me, senpai!',
-          });
-        }
-
-        
-
+     
+      if (response.length === 1) {
+        dropdown.innerHTML = card(response[0]);
+      } else if (response.length < 11) {
+        dropdown.innerHTML = list({ response });
+      } else {
+        alert({
+          text: 'ведіть більше даних'
+        });
+      }
     })
-
+      .catch(function(){
+        alert({
+          text: 'некоректно введені дані, або країни незнайдено'
+        });
+      })
 
 
 }
